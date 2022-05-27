4e9b4ffdbb766c6bccbbec7c605b651f51f4ecba
Alison Turner <alisonmoons@gmail.com>
2022-05-27 11:10:06 -0300

    Added Pressure Valves

---
+++ src/main/java/Snowpunk/cards/PressureValves.java
@@ -0,0 +1,56 @@
+package Snowpunk.cards;
+
+import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.powers.PressureValvesPower;
+import Snowpunk.util.Wiz;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.relics.ChemicalX;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class PressureValves extends AbstractEasyCard {
+    public final static String ID = makeID("PressureValves");
+
+    private static final AbstractCard.CardRarity RARITY = AbstractCard.CardRarity.UNCOMMON;
+    private static final AbstractCard.CardTarget TARGET = AbstractCard.CardTarget.SELF;
+    private static final AbstractCard.CardType TYPE = AbstractCard.CardType.POWER;
+
+    private static final int COST = 2;
+    private static final int UP_COST = -1;
+    private static final int STACKS = 2;
+
+    public PressureValves() {
+        super(ID, COST, TYPE, RARITY, TARGET);
+        baseMagicNumber = magicNumber = STACKS;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (upgraded) {
+            int effect = this.energyOnUse;
+
+            if (p.hasRelic("Chemical X")) {
+                effect += ChemicalX.BOOST;
+                p.getRelic("Chemical X").flash();
+            }
+
+            effect += magicNumber;
+
+            if (effect > 0) {
+                Wiz.applyToSelf(new PressureValvesPower(p, effect));
+            }
+
+            if (!this.freeToPlayOnce) {
+                p.energy.use(EnergyPanel.totalCount);
+            }
+        } else {
+            Wiz.applyToSelf(new PressureValvesPower(p, magicNumber));
+        }
+    }
+
+    public void upp() {
+        upgradeBaseCost(UP_COST);
+    }
+}
---
+++ src/main/java/Snowpunk/powers/PressureValvesPower.java
@@ -0,0 +1,29 @@
+package Snowpunk.powers;
+
+import Snowpunk.powers.interfaces.ModifyPressurePower;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class PressureValvesPower extends AbstractEasyPower implements ModifyPressurePower {
+    public static String POWER_ID = makeID("PressureValvesPower");
+    public static PowerStrings strings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static String[] DESCRIPTIONS = strings.DESCRIPTIONS;
+
+    public PressureValvesPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, strings.NAME, PowerType.BUFF, false, owner, amount);
+        this.loadRegion("panache");
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public int modifyPressure(int pressure) {
+        return pressure + amount;
+    }
+}
