d0a67b94cb9f5a80e054da85144f862a2587e6ed
Alison Turner <alisonmoons@gmail.com>
2022-05-29 12:15:33 -0300

    Add Coolant Valve

---
+++ src/main/java/Snowpunk/cards/parts/CoolantValve.java
@@ -0,0 +1,30 @@
+package Snowpunk.cards.parts;
+
+import Snowpunk.patches.CardTemperatureFields;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+import java.util.function.Predicate;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class CoolantValve extends AbstractPartCard {
+    public static final String ID = makeID(CoolantValve.class.getSimpleName());
+
+    private static final AbstractCard.CardType TYPE = CardType.SKILL;
+    private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
+
+    public CoolantValve() {
+        super(ID, TYPE, RARITY);
+        CardTemperatureFields.addInherentHeat(this, -1);
+    }
+
+    @Override
+    public Predicate<AbstractCard> getFilter() {
+        return isPlayable.and(c -> CardTemperatureFields.getCardHeat(c) != -2);
+    }
+
+    @Override
+    public void apply(AbstractCard card) {
+        CardTemperatureFields.addHeat(card, -1);
+    }
+}
