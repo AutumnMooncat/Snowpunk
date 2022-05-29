2b6d8fd0abd600a175b7bbc3ed32abf367c57180
Alison Turner <alisonmoons@gmail.com>
2022-05-29 12:27:00 -0300

    Add Oiled Gears

---
+++ src/main/java/Snowpunk/cardmods/DrawAndDiscardRandomMod.java
@@ -0,0 +1,62 @@
+package Snowpunk.cardmods;
+
+import Snowpunk.util.Wiz;
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class DrawAndDiscardRandomMod extends AbstractCardModifier {
+    public static final String ID = makeID(DrawAndDiscardRandomMod.class.getSimpleName());
+    public static String[] TEXT = CardCrawlGame.languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION;
+    int amount;
+
+    public DrawAndDiscardRandomMod(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(rawDescription).append(TEXT[0]).append(amount);
+        if (amount == 1) {
+            sb.append(TEXT[1]).append(TEXT[3]).append(amount).append(TEXT[4]);
+        } else {
+            sb.append(TEXT[2]).append(TEXT[3]).append(amount).append(TEXT[5]);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        Wiz.atb(new DrawCardAction(amount));
+        Wiz.atb(new DiscardAction(Wiz.adp(), Wiz.adp(), amount, true));
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        if (CardModifierManager.hasModifier(card, ID)) {
+            DrawAndDiscardRandomMod mod = (DrawAndDiscardRandomMod) CardModifierManager.getModifiers(card, ID).get(0);
+            mod.amount += amount;
+            card.initializeDescription();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new DrawAndDiscardRandomMod(amount);
+    }
+}
---
+++ src/main/java/Snowpunk/cards/parts/OiledGears.java
@@ -0,0 +1,32 @@
+package Snowpunk.cards.parts;
+
+import Snowpunk.cardmods.DrawAndDiscardRandomMod;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+import java.util.function.Predicate;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class OiledGears extends AbstractPartCard {
+    public static final String ID = makeID(OiledGears.class.getSimpleName());
+    public static String[] TEXT = CardCrawlGame.languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION;
+
+    private static final AbstractCard.CardType TYPE = CardType.SKILL;
+    private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
+
+    public OiledGears() {
+        super(ID, TYPE, RARITY);
+    }
+
+    @Override
+    public Predicate<AbstractCard> getFilter() {
+        return isPlayable;
+    }
+
+    @Override
+    public void apply(AbstractCard card) {
+        CardModifierManager.addModifier(card, new DrawAndDiscardRandomMod(1));
+    }
+}
