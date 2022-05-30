b3ecde36c616f65952abde969a77a528353fc92c
Alison Turner <alisonmoons@gmail.com>
2022-05-30 16:21:52 -0300

    Add Cauterize

---
+++ src/main/java/Snowpunk/cards/Cauterize.java
@@ -0,0 +1,38 @@
+package Snowpunk.cards;
+
+import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.damageMods.CauterizeDamage;
+import Snowpunk.powers.BurnPower;
+import Snowpunk.util.Wiz;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class Cauterize extends AbstractEasyCard {
+    public final static String ID = makeID(Cauterize.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+
+    private static final int COST = 2;
+    private static final int DMG = 7;
+    private static final int UP_DMG = 3;
+
+    public Cauterize() {
+        super(ID, COST, TYPE, RARITY, TARGET);
+        baseDamage = damage = DMG;
+        DamageModifierManager.addModifier(this, new CauterizeDamage());
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.FIRE);
+    }
+
+    public void upp() {
+        upgradeDamage(UP_DMG);
+    }
+}
---
+++ src/main/java/Snowpunk/damageMods/CauterizeDamage.java
@@ -0,0 +1,28 @@
+package Snowpunk.damageMods;
+
+import Snowpunk.powers.BurnPower;
+import Snowpunk.util.Wiz;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+
+public class CauterizeDamage extends AbstractDamageModifier {
+
+    @Override
+    public void onLastDamageTakenUpdate(DamageInfo info, int lastDamageTaken, int overkillAmount, AbstractCreature target) {
+        if (lastDamageTaken > 0) {
+            Wiz.atb(new ApplyPowerAction(target, info.owner, new BurnPower(target, info.owner, lastDamageTaken)));
+        }
+    }
+
+    @Override
+    public boolean isInherent() {
+        return true;
+    }
+
+    @Override
+    public AbstractDamageModifier makeCopy() {
+        return new CauterizeDamage();
+    }
+}
