3387374117dc6fd70c4bab55d3cc4d27aaf6cdc6
Alison Turner <alisonmoons@gmail.com>
2022-05-30 16:34:55 -0300

    Add Black Ice

---
+++ src/main/java/Snowpunk/cards/BlackIce.java
@@ -0,0 +1,47 @@
+package Snowpunk.cards;
+
+import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.powers.FrostbitePower;
+import Snowpunk.powers.SnowballPower;
+import Snowpunk.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class BlackIce extends AbstractEasyCard {
+    public final static String ID = makeID(BlackIce.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+
+    private static final int COST = 0;
+
+    public BlackIce() {
+        super(ID, COST, TYPE, RARITY, TARGET);
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (getSnow() > 0) {
+            for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+                if (!mo.isDeadOrEscaped()) {
+                    if (upgraded) {
+                        Wiz.applyToEnemy(mo, new FrostbitePower(mo, p, getSnow()));
+                    }
+                    Wiz.applyToEnemy(mo, new WeakPower(mo, getSnow(), false));
+                    Wiz.applyToEnemy(mo, new VulnerablePower(mo, getSnow(), false));
+                }
+            }
+            Wiz.atb(new RemoveSpecificPowerAction(p, p, SnowballPower.POWER_ID));
+        }
+    }
+
+    public void upp() {}
+}
