77fdd5cb344b178cecefe2880623236bfd2f9b08
Alison Turner <alisonmoons@gmail.com>
2022-05-31 12:16:03 -0300

    Cauterize now Hot and Uncommon. Damage raised from 7(10) to 8(11)

--- src/main/java/Snowpunk/cards/Cauterize.java
+++ src/main/java/Snowpunk/cards/Cauterize.java
@@ -2,6 +2,7 @@ package Snowpunk.cards;
 
 import Snowpunk.cards.abstracts.AbstractEasyCard;
 import Snowpunk.damageMods.CauterizeDamage;
+import Snowpunk.patches.CardTemperatureFields;
 import Snowpunk.powers.BurnPower;
 import Snowpunk.util.Wiz;
 import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
@@ -14,17 +15,18 @@ import static Snowpunk.SnowpunkMod.makeID;
 public class Cauterize extends AbstractEasyCard {
     public final static String ID = makeID(Cauterize.class.getSimpleName());
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 2;
-    private static final int DMG = 7;
+    private static final int DMG = 8;
     private static final int UP_DMG = 3;
 
     public Cauterize() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
+        CardTemperatureFields.addInherentHeat(this, 1);
         DamageModifierManager.addModifier(this, new CauterizeDamage());
     }
 
