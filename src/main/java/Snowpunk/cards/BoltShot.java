364d5e362f5a3034fece5a9c522d04db5c14d225
Alison Turner <alisonmoons@gmail.com>
2022-06-08 15:42:06 -0300

    Updated Bolt Shot to match design doc

--- src/main/java/Snowpunk/cards/BoltShot.java
+++ src/main/java/Snowpunk/cards/BoltShot.java
@@ -7,6 +7,7 @@ import Snowpunk.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
@@ -18,17 +19,20 @@ public class BoltShot extends AbstractEasyCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 1;
-    private static final int DMG = 10;
-    private static final int UP_DMG = 4;
+    private static final int DMG = 8;
+    private static final int UP_DMG = 3;
+    private static final int VULN = 1;
 
     public BoltShot() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
+        baseMagicNumber = magicNumber = VULN;
         tags.add(CustomTags.GUN);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
     }
 
     public void upp() {
