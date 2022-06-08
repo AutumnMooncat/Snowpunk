c0fb4aa3c0813a5b257dc89023f95d3a622e4def
Alison Turner <alisonmoons@gmail.com>
2022-06-08 08:38:17 -0300

    Add Better Exhaust mod that has higher priority, so it appears after other mods

---
+++ src/main/java/Snowpunk/cardmods/BetterExhaustMod.java
@@ -0,0 +1,41 @@
+package Snowpunk.cardmods;
+
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class BetterExhaustMod extends AbstractCardModifier {
+    public static final String ID = makeID(BetterExhaustMod.class.getSimpleName());
+    public static String[] TEXT = CardCrawlGame.languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION;
+
+    public BetterExhaustMod() {
+        this.priority = 2;
+    }
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.exhaust = true;
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + TEXT[0];
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        return !card.exhaust;
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new BetterExhaustMod();
+    }
+}
