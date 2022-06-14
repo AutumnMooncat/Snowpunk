207bfff8ad7ce10693720fee70d7007edf1fd125
Alison Turner <alisonmoons@gmail.com>
2022-06-14 09:56:16 -0300

    Update Chuck Tools to match design doc

--- src/main/java/Snowpunk/cards/ChuckTools.java
+++ src/main/java/Snowpunk/cards/ChuckTools.java
@@ -1,47 +1,47 @@
 package Snowpunk.cards;
 
-import Snowpunk.actions.TinkerAction;
 import Snowpunk.cards.abstracts.AbstractEasyCard;
 import Snowpunk.powers.TinkerNextCardPower;
 import Snowpunk.util.Wiz;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
 public class ChuckTools extends AbstractEasyCard {
     public final static String ID = makeID(ChuckTools.class.getSimpleName());
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
     private static final int UP_COST = 0;
-    private static final int EFFECT = 1;
+    private static final int EFFECT = 2;
     private static final int UP_EFFECT = 1;
+    private static final int TINKER = 1;
+    private static final int UP_TINKER = 1;
 
     public ChuckTools() {
         super(ID, COST, TYPE, RARITY, TARGET);
-        magicNumber = baseMagicNumber = EFFECT;
+        secondMagic = baseSecondMagic = EFFECT;
+        magicNumber = baseMagicNumber = TINKER;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
             if (!mo.isDeadOrEscaped()) {
-                Wiz.applyToEnemy(mo, new VulnerablePower(mo, magicNumber, false));
-                Wiz.applyToEnemy(mo, new WeakPower(mo, magicNumber, false));
+                Wiz.applyToEnemy(mo, new VulnerablePower(mo, secondMagic, false));
+                //Wiz.applyToEnemy(mo, new WeakPower(mo, secondMagic, false));
             }
         }
-        Wiz.applyToSelf(new TinkerNextCardPower(p, 1));
+        Wiz.applyToSelf(new TinkerNextCardPower(p, magicNumber));
     }
 
     public void upp() {
-        upgradeBaseCost(UP_COST);
+        //upgradeBaseCost(UP_COST);
+        upgradeMagicNumber(UP_TINKER);
     }
 }
