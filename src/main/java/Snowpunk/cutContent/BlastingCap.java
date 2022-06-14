bf89dcc13aed2bec4c0d2ebeacb733db52b75d26
Alison Turner <alisonmoons@gmail.com>
2022-06-14 09:55:59 -0300

    Move Blasting Cap to cut content

--- src/main/java/Snowpunk/cutContent/BlastingCap.java
+++ src/main/java/Snowpunk/cutContent/BlastingCap.java
@@ -1,7 +1,6 @@
-package Snowpunk.cards;
+package Snowpunk.cutContent;
 
 import Snowpunk.cards.abstracts.AbstractEasyCard;
-import Snowpunk.patches.CustomTags;
 import Snowpunk.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
