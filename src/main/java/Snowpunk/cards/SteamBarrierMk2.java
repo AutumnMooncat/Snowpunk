8df06cdc497f5fe068c6bc3a6777aa4db322820f
Alison Turner <alisonmoons@gmail.com>
2022-07-01 19:37:21 -0300

    Steam Barrier Mk2 now spawns with Mk2 mod

--- src/main/java/Snowpunk/cards/SteamBarrierMk2.java
+++ src/main/java/Snowpunk/cards/SteamBarrierMk2.java
@@ -1,21 +1,15 @@
 package Snowpunk.cards;
 
-import Snowpunk.actions.ModCardTempAction;
-import Snowpunk.cardmods.TemperatureMod;
+import Snowpunk.cardmods.MkMod;
 import Snowpunk.cards.abstracts.AbstractEasyCard;
 import Snowpunk.patches.CardTemperatureFields;
-import Snowpunk.util.Wiz;
 import basemod.helpers.CardModifierManager;
-import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class SteamBarrierMk2 extends AbstractEasyCard
-{
+public class SteamBarrierMk2 extends AbstractEasyCard {
     public final static String ID = makeID(SteamBarrierMk2.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
@@ -24,20 +18,18 @@ public class SteamBarrierMk2 extends AbstractEasyCard
 
     private static final int COST = 1, BLOCK = 8, UPG_BLOCK = 3;
 
-    public SteamBarrierMk2()
-    {
+    public SteamBarrierMk2() {
         super(ID, COST, TYPE, RARITY, TARGET);
         block = baseBlock = BLOCK;
         CardTemperatureFields.addInherentHeat(this, 1);
+        CardModifierManager.addModifier(this, new MkMod(2));
     }
 
-    public void use(AbstractPlayer p, AbstractMonster m)
-    {
+    public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
     }
 
-    public void upp()
-    {
+    public void upp() {
         upgradeBlock(UPG_BLOCK);
         upgradedBlock = true;
         initializeDescription();
