53f1a135b356ff3c260578f9cdf232f98fa9a4a3
Alison Turner <alisonmoons@gmail.com>
2022-07-17 10:00:58 -0300

    Add Multiupgrades to Avalanche

--- src/main/java/Snowpunk/cards/Avalanche.java
+++ src/main/java/Snowpunk/cards/Avalanche.java
@@ -1,9 +1,8 @@
 package Snowpunk.cards;
 
-import Snowpunk.cardmods.TemperatureMod;
-import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cardmods.FrostMod;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.patches.CardTemperatureFields;
-import Snowpunk.util.SteamEngine;
 import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -12,7 +11,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class Avalanche extends AbstractEasyCard {
+public class Avalanche extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(Avalanche.class.getSimpleName());
 
     private static final AbstractCard.CardRarity RARITY = CardRarity.UNCOMMON;
@@ -20,32 +19,42 @@ public class Avalanche extends AbstractEasyCard {
     private static final AbstractCard.CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 2;
+    private static final int UP_COST = 3;
     private static final int DMG = 12;
-    private static final int UP_DMG = 3;
+    private static final int UP_DMG = 4;
+    private static final int SCALE = 2;
+    private static final int UP_SCALE = 2;
 
     public Avalanche() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
         isMultiDamage = true;
         CardTemperatureFields.addInherentHeat(this, -2);
+        CardModifierManager.addModifier(this, new FrostMod());
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         allDmg(AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        if (SteamEngine.heat == SteamEngine.FREEZING) {
-            allDmg(AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+    }
+
+    @Override
+    public void onRetained() {
+        if (magicNumber > 0) {
+            this.upgradeDamage(this.magicNumber);
         }
     }
 
     @Override
-    public void triggerOnGlowCheck() {
-        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        if (SteamEngine.heat == SteamEngine.FREEZING) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        }
-    }
-
-    public void upp() {
-        upgradeDamage(UP_DMG);
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeDamage(UP_DMG));
+        addUpgradeData(this, () -> {
+            baseMagicNumber = magicNumber = 0;
+            upgradeMagicNumber(SCALE);
+        });
+        addUpgradeData(this, () -> {
+            upgradeBaseCost(UP_COST);
+            upgradeDamage(UP_DMG);
+            upgradeMagicNumber(UP_SCALE);
+        }, 0, 1);
     }
 }
