0b14cfb9e809aa9fe1a2a37d44f504617bfae9d1
Alison Turner <alisonmoons@gmail.com>
2022-07-28 12:45:32 -0300

    Add multiupgrades to ArtilleryCargo

--- src/main/java/Snowpunk/cards/ArtilleryCargo.java
+++ src/main/java/Snowpunk/cards/ArtilleryCargo.java
@@ -1,8 +1,11 @@
 package Snowpunk.cards;
 
+import Snowpunk.cardmods.VentMod;
 import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.patches.CustomTags;
 import Snowpunk.util.Wiz;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,7 +13,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class ArtilleryCargo extends AbstractEasyCard {
+public class ArtilleryCargo extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(ArtilleryCargo.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.RARE;
@@ -27,10 +30,20 @@ public class ArtilleryCargo extends AbstractEasyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.atb(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.FIRE));
+        if (m != null) {
+            dmg(m, AbstractGameAction.AttackEffect.FIRE);
+        } else {
+            Wiz.atb(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.FIRE));
+        }
     }
 
-    public void upp() {
-        upgradeDamage(UP_DMG);
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeDamage(UP_DMG));
+        addUpgradeData(this, () -> {
+            this.target = CardTarget.ENEMY;
+            uDesc();
+        });
+        addUpgradeData(this, () -> CardModifierManager.addModifier(this, new VentMod()));
     }
 }
