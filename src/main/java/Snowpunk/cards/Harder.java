2c30fa428cdd37209f55cffd9aef4407ef8e62fa
Alison Turner <alisonmoons@gmail.com>
2022-07-27 21:31:02 -0300

    Add multiupgrades to Harder

--- src/main/java/Snowpunk/cards/Harder.java
+++ src/main/java/Snowpunk/cards/Harder.java
@@ -1,18 +1,18 @@
 package Snowpunk.cards;
 
-import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
+import Snowpunk.damageMods.ProtectionOnKillDamage;
 import Snowpunk.patches.CardTemperatureFields;
-import Snowpunk.patches.CustomTags;
-import Snowpunk.powers.IceCubePower;
+import Snowpunk.powers.ChillPower;
 import Snowpunk.util.Wiz;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class Harder extends AbstractEasyCard {
+public class Harder extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(Harder.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.RARE;
@@ -20,7 +20,7 @@ public class Harder extends AbstractEasyCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 1;
-    private static final int DMG = 10;
+    private static final int DMG = 12;
     private static final int UP_DMG = 4;
     private static final int EFFECT = 2;
     private static final int UP_EFFECT = 1;
@@ -34,11 +34,16 @@ public class Harder extends AbstractEasyCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SMASH);
-        Wiz.applyToSelf(new IceCubePower(m, magicNumber));
+        Wiz.applyToEnemy(m, new ChillPower(m, magicNumber));
     }
 
-    public void upp() {
-        upgradeDamage(UP_DMG);
-        upgradeMagicNumber(UP_EFFECT);
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeDamage(UP_DMG));
+        addUpgradeData(this, () -> upgradeMagicNumber(UP_EFFECT));
+        addUpgradeData(this, () -> {
+            DamageModifierManager.addModifier(this, new ProtectionOnKillDamage());
+            uDesc();
+        });
     }
 }
