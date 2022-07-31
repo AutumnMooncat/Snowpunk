6320106b139a861dbd16e7264b20b6f1b0260812
Alison Turner <alisonmoons@gmail.com>
2022-07-31 19:47:32 -0300

    Add multiupgrades to Pressure Valves and Brass Knuckles

--- src/main/java/Snowpunk/cards/BrassKnuckles.java
+++ src/main/java/Snowpunk/cards/BrassKnuckles.java
@@ -1,6 +1,7 @@
 package Snowpunk.cards;
 
 import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.cards.interfaces.OnTinkeredCard;
 import Snowpunk.cards.parts.AbstractPartCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -9,7 +10,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class BrassKnuckles extends AbstractEasyCard implements OnTinkeredCard {
+public class BrassKnuckles extends AbstractMultiUpgradeCard implements OnTinkeredCard {
     public final static String ID = makeID(BrassKnuckles.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
@@ -17,9 +18,13 @@ public class BrassKnuckles extends AbstractEasyCard implements OnTinkeredCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 1;
+    private static final int UP_COST = 0;
     private static final int DMG = 4;
     private static final int UP_DMG = 2;
+    private static final int DOWN_DMG = -1;
     private static final int BOOST = 2;
+
+    private boolean third = false;
 
     public BrassKnuckles() {
         super(ID, COST, TYPE, RARITY, TARGET);
@@ -30,15 +35,25 @@ public class BrassKnuckles extends AbstractEasyCard implements OnTinkeredCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-    }
-
-    public void upp() {
-        upgradeDamage(UP_DMG);
+        if (third) {
+            dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        }
     }
 
     @Override
     public void onTinkered(AbstractPartCard part) {
         upgradeDamage(magicNumber);
         superFlash();
+    }
+
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeDamage(UP_DMG));
+        addUpgradeData(this, () -> upgradeBaseCost(UP_COST));
+        addUpgradeData(this, () -> {
+            third = true;
+            upgradeDamage(DOWN_DMG);
+            uDesc();
+        });
     }
 }
