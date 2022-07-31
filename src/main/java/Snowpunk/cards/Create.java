78634198e9f7d148aa446788659396ec1136209c
Alison Turner <alisonmoons@gmail.com>
2022-07-31 11:46:19 -0300

    Add Breakthrough. Add multiupgrades to Create

--- src/main/java/Snowpunk/cards/Create.java
+++ src/main/java/Snowpunk/cards/Create.java
@@ -1,7 +1,7 @@
 package Snowpunk.cards;
 
 import Snowpunk.actions.AssembleCardAction;
-import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.util.Wiz;
 import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.FleetingField;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -9,7 +9,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class Create extends AbstractEasyCard {
+public class Create extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(Create.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
@@ -17,6 +17,10 @@ public class Create extends AbstractEasyCard {
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
+    private static final int UP_COST = 0;
+
+    private boolean machination = false;
+    private boolean random = false;
 
     public Create() {
         super(ID, COST, TYPE, RARITY, TARGET);
@@ -25,14 +29,39 @@ public class Create extends AbstractEasyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (upgraded) {
-            Wiz.atb(new AssembleCardAction(AssembleCardAction.AssembleType.MACHINATION));
+        if (machination) {
+            Wiz.atb(new AssembleCardAction(AssembleCardAction.AssembleType.MACHINATION, !random, random));
         } else {
-            Wiz.atb(new AssembleCardAction(AssembleCardAction.AssembleType.CREATION));
+            Wiz.atb(new AssembleCardAction(AssembleCardAction.AssembleType.CREATION, !random, random));
         }
     }
 
     public void upp() {
         uDesc();
+    }
+
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this, () -> {
+            machination = true;
+            if (random) {
+                rawDescription = cardStrings.EXTENDED_DESCRIPTION[1];
+                initializeDescription();
+            } else {
+                uDesc();
+            }
+        });
+        addUpgradeData(this, () -> {
+            random = true;
+            FleetingField.fleeting.set(this, false);
+            if (machination) {
+                rawDescription = cardStrings.EXTENDED_DESCRIPTION[1];
+                initializeDescription();
+            } else {
+                rawDescription = cardStrings.EXTENDED_DESCRIPTION[0];
+                initializeDescription();
+            }
+        });
+        addUpgradeData(this, () -> upgradeBaseCost(UP_COST), 1);
     }
 }
