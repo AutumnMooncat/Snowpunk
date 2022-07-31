dbf045e8f0c40ae534d5d1d5078265d1f77cfe2d
Alison Turner <alisonmoons@gmail.com>
2022-07-31 15:59:41 -0300

    Clean IcicleSpear code

--- src/main/java/Snowpunk/cards/IcicleSpear.java
+++ src/main/java/Snowpunk/cards/IcicleSpear.java
@@ -2,17 +2,14 @@ package Snowpunk.cards;
 
 import Snowpunk.actions.ModCardTempAction;
 import Snowpunk.cardmods.FrostMod;
-import Snowpunk.cards.abstracts.AbstractEasyCard;
 import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.patches.CardTemperatureFields;
 import Snowpunk.powers.ChillPower;
-import Snowpunk.powers.FrostbitePower;
 import Snowpunk.util.Wiz;
 import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
@@ -25,30 +22,31 @@ public class IcicleSpear extends AbstractMultiUpgradeCard {
 
     private static final int COST = 1, DMG = 6, UP_DMG = 3, MAGIC = 3;
 
+    private boolean chill = false;
+
     public IcicleSpear() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
-        magicNumber = baseMagicNumber = MAGIC;
-        info = baseInfo = 0;
         CardModifierManager.addModifier(this, new FrostMod());
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
         Wiz.atb(new ModCardTempAction(this, -1));
-        if (info > 0)
+        if (chill) {
             Wiz.applyToEnemy(m, new ChillPower(m, magicNumber));
+        }
     }
 
     @Override
     public void addUpgrades() {
         addUpgradeData(this, () -> upgradeDamage(UP_DMG));
-        addUpgradeData(this, () -> upgrade2());
+        addUpgradeData(this, () -> {
+            chill = true;
+            baseMagicNumber = magicNumber = 0;
+            upgradeMagicNumber(MAGIC);
+            uDesc();
+        });
         addUpgradeData(this, () -> CardTemperatureFields.addInherentHeat(this, -1));
-    }
-
-    private void upgrade2() {
-        upgradeInfo(1);
-        uDesc();
     }
 }
