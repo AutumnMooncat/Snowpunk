565331897de6be086a6b41d3fe1ae5daf987cbce
Alison Turner <alisonmoons@gmail.com>
2022-07-31 16:49:50 -0300

    Add multiupgrades to Comburstion

--- src/main/java/Snowpunk/cards/Comburstion.java
+++ src/main/java/Snowpunk/cards/Comburstion.java
@@ -1,6 +1,7 @@
 package Snowpunk.cards;
 
 import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.patches.CardTemperatureFields;
 import Snowpunk.powers.BurnPower;
 import Snowpunk.util.SteamEngine;
@@ -14,7 +15,7 @@ import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class Comburstion extends AbstractEasyCard {
+public class Comburstion extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(Comburstion.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
@@ -22,9 +23,10 @@ public class Comburstion extends AbstractEasyCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 2;
-    private static final int DMG = 16;
-    private static final int UP_DMG = 6;
-    private static final int BURN = 8;
+    private static final int DMG = 10;
+    private static final int UP_DMG = 4;
+    private static final int BURN = 5;
+    private static final int UP_BURN = 2;
 
     public Comburstion() {
         super(ID, COST, TYPE, RARITY, TARGET);
@@ -35,24 +37,17 @@ public class Comburstion extends AbstractEasyCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         allDmg(AbstractGameAction.AttackEffect.FIRE);
-        if (SteamEngine.heat == SteamEngine.OVERHEATED) {
-            for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
-                if (!mo.isDeadOrEscaped()) {
-                    Wiz.applyToEnemy(mo, new BurnPower(mo, p, magicNumber));
-                }
+        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+            if (!mo.isDeadOrEscaped()) {
+                Wiz.applyToEnemy(mo, new BurnPower(mo, p, magicNumber));
             }
         }
     }
 
     @Override
-    public void triggerOnGlowCheck() {
-        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        if (SteamEngine.heat == SteamEngine.OVERHEATED) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        }
-    }
-
-    public void upp() {
-        upgradeDamage(UP_DMG);
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeDamage(UP_DMG));
+        addUpgradeData(this, () -> upgradeMagicNumber(UP_BURN));
+        addUpgradeData(this, () -> CardTemperatureFields.addInherentHeat(this, 1));
     }
 }
