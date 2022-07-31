e65a2023dd62cb1cb89e55559f44d7320a962967
Alison Turner <alisonmoons@gmail.com>
2022-07-31 16:36:35 -0300

    Updated Winter Storm to new effect

--- src/main/java/Snowpunk/cards/WinterStorm.java
+++ src/main/java/Snowpunk/cards/WinterStorm.java
@@ -1,6 +1,9 @@
 package Snowpunk.cards;
 
+import Snowpunk.actions.ModEngineTempAction;
 import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
+import Snowpunk.patches.CardTemperatureFields;
 import Snowpunk.powers.WinterStormPower;
 import Snowpunk.util.Wiz;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -8,7 +11,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class WinterStorm extends AbstractEasyCard {
+public class WinterStorm extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(WinterStorm.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
@@ -16,8 +19,11 @@ public class WinterStorm extends AbstractEasyCard {
     private static final CardType TYPE = CardType.POWER;
 
     private static final int COST = 2;
-    private static final int STACKS = 2;
+    private static final int UP_COST = 1;
+    private static final int STACKS = 1;
     private static final int UP_STACKS = 1;
+
+    private boolean freeze = false;
 
     public WinterStorm() {
         super(ID, COST, TYPE, RARITY, TARGET);
@@ -25,10 +31,20 @@ public class WinterStorm extends AbstractEasyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (freeze) {
+            Wiz.atb(new ModEngineTempAction(-99));
+        }
         Wiz.applyToSelf(new WinterStormPower(p, magicNumber));
     }
 
-    public void upp() {
-        upgradeMagicNumber(UP_STACKS);
+
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeBaseCost(UP_COST));
+        addUpgradeData(this, () -> CardTemperatureFields.addInherentHeat(this, -1));
+        addUpgradeData(this, () -> {
+            this.freeze = true;
+            uDesc();
+        });
     }
 }
