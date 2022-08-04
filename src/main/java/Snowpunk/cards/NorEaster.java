2a36d578b3be00b189cd355f74b71fdaeacc8b38
Alison Turner <alisonmoons@gmail.com>
2022-08-04 13:37:19 -0300

    Add Multiupgrades to Noreaster

--- src/main/java/Snowpunk/cards/NorEaster.java
+++ src/main/java/Snowpunk/cards/NorEaster.java
@@ -6,6 +6,8 @@ import Snowpunk.actions.ModEngineStabilityPointAction;
 import Snowpunk.actions.ModEngineTempAction;
 import Snowpunk.cardmods.TemperatureMod;
 import Snowpunk.cards.abstracts.AbstractEasyCard;
+import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
+import Snowpunk.patches.CardTemperatureFields;
 import Snowpunk.util.SteamEngine;
 import Snowpunk.util.Wiz;
 import basemod.helpers.CardModifierManager;
@@ -14,8 +16,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Snowpunk.SnowpunkMod.makeID;
 
-public class NorEaster extends AbstractEasyCard
-{
+public class NorEaster extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(NorEaster.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.RARE;
@@ -23,27 +24,35 @@ public class NorEaster extends AbstractEasyCard
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
+    private static final int UP_COST = 0;
 
-    public NorEaster()
-    {
+    private boolean allCards = false;
+
+    public NorEaster() {
         super(ID, COST, TYPE, RARITY, TARGET);
         exhaust = true;
+        CardTemperatureFields.addInherentHeat(this, -2);
     }
 
-    public void use(AbstractPlayer p, AbstractMonster m)
-    {
-        Wiz.atb(new ModCardTempEverywhereAction(-99));
-        Wiz.atb(new ModCardTempAction(this, -99));
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (allCards) {
+            Wiz.atb(new ModCardTempEverywhereAction(-99));
+        } else {
+            Wiz.atb(new ModCardTempAction(-1, -99, true));
+        }
         Wiz.atb(new ModEngineTempAction(-99));
-        if (upgraded)
-            Wiz.atb(new ModEngineStabilityPointAction(-99));
     }
 
-    public void upp()
-    {
-        upgradeBaseCost(0);
-        upgradedCost = true;
-        rawDescription = cardStrings.UPGRADE_DESCRIPTION;
-        initializeDescription();
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this, () -> upgradeBaseCost(UP_COST));
+        addUpgradeData(this, () -> {
+            this.isInnate = true;
+            this.baseInfo = this.info = 1;
+        });
+        addUpgradeData(this, () -> {
+            allCards = true;
+            uDesc();
+        });
     }
 }
