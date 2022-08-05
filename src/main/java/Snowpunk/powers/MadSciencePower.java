5dd6ff8882d1cac04e2813d3dfa5fdee135f1177
Alison Turner <alisonmoons@gmail.com>
2022-08-05 14:57:28 -0300

    Add Mad Science. Clarify that upgrades are done for the rest of combat

---
+++ src/main/java/Snowpunk/powers/MadSciencePower.java
@@ -0,0 +1,37 @@
+package Snowpunk.powers;
+
+import Snowpunk.util.Wiz;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.UpgradeRandomCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class MadSciencePower extends AbstractEasyPower {
+    public static String POWER_ID = makeID(MadSciencePower.class.getSimpleName());
+    public static PowerStrings strings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static String[] DESCRIPTIONS = strings.DESCRIPTIONS;
+
+    public MadSciencePower(AbstractCreature owner, int amount) {
+        super(POWER_ID, strings.NAME, PowerType.BUFF, false, owner, amount);
+        this.loadRegion("master_reality");
+    }
+
+    @Override
+    public void onPlayCard(AbstractCard card, AbstractMonster m) {
+        this.addToBot(new UpgradeRandomCardAction());
+    }
+
+    @Override
+    public void updateDescription() {
+        if (amount == 1) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        } else {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[2];
+        }
+    }
+}
