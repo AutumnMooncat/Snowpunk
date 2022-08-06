631d4f61c6b7b3101cf51ad70ea1c7b59d1c6574
Alison Turner <alisonmoons@gmail.com>
2022-08-06 20:07:56 -0300

    Rework Refrigerate

--- src/main/java/Snowpunk/cards/Refrigerate.java
+++ src/main/java/Snowpunk/cards/Refrigerate.java
@@ -5,6 +5,8 @@ import Snowpunk.cardmods.TemperatureMod;
 import Snowpunk.cardmods.parts.ReshuffleMod;
 import Snowpunk.cards.abstracts.AbstractEasyCard;
 import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
+import Snowpunk.patches.CardTemperatureFields;
+import Snowpunk.powers.RefrigeratePower;
 import Snowpunk.util.Wiz;
 import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -21,38 +23,26 @@ public class Refrigerate extends AbstractMultiUpgradeCard {
     private static final AbstractCard.CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
-    private static final int BLK = 8;
+    private static final int BLK = 7;
     private static final int UP_BLK = 3;
-    private static final int BUFF = 3;
-
-    private boolean buffOnRetain = false;
+    private static final int EFFECT = 1;
+    private static final int UP_EFFECT = 1;
 
     public Refrigerate() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseBlock = block = BLK;
+        magicNumber = baseMagicNumber = EFFECT;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        Wiz.atb(new ModCardTempAction(this, -1));
-    }
-
-    @Override
-    public void onRetained() {
-        if (buffOnRetain) {
-            upgradeBlock(magicNumber);
-        }
+        Wiz.applyToSelf(new RefrigeratePower(p, magicNumber));
     }
 
     @Override
     public void addUpgrades() {
         addUpgradeData(this, () -> upgradeBlock(UP_BLK));
-        addUpgradeData(this, () -> CardModifierManager.addModifier(this, new ReshuffleMod()));
-        addUpgradeData(this, () -> {
-            buffOnRetain = true;
-            baseMagicNumber = magicNumber = 0;
-            upgradeMagicNumber(BUFF);
-            uDesc();
-        });
+        addUpgradeData(this, () -> CardTemperatureFields.addInherentHeat(this, -1));
+        addUpgradeData(this, () -> upgradeMagicNumber(UP_EFFECT));
     }
 }
