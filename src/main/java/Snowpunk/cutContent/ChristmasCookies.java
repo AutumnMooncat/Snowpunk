f9f55af1ce80ed3a3e4de57b514ddc5b0cbd51ce
Alison Turner <alisonmoons@gmail.com>
2022-08-05 21:57:03 -0300

    Cut ChristmasCookies from the draft

--- src/main/java/Snowpunk/cutContent/ChristmasCookies.java
+++ src/main/java/Snowpunk/cutContent/ChristmasCookies.java
@@ -1,12 +1,9 @@
-package Snowpunk.cards;
+package Snowpunk.cutContent;
 
 import Snowpunk.actions.HolidayCheerUpAction;
-import Snowpunk.cardmods.parts.ReshuffleMod;
 import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.powers.ChristmasCookiePower;
 import Snowpunk.util.Wiz;
-import basemod.helpers.CardModifierManager;
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
