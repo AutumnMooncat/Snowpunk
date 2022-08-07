07bb83e3ff0beeedc84da38df9c1356656f0f4d1
Alison Turner <alisonmoons@gmail.com>
2022-08-07 14:20:16 -0300

    Rework Better

--- src/main/java/Snowpunk/cards/Better.java
+++ src/main/java/Snowpunk/cards/Better.java
@@ -2,10 +2,7 @@ package Snowpunk.cards;
 
 import Snowpunk.cards.abstracts.AbstractMultiUpgradeCard;
 import Snowpunk.patches.CardTemperatureFields;
-import Snowpunk.powers.BetterCreationPower;
-import Snowpunk.powers.BetterInventionPower;
-import Snowpunk.powers.BetterMachinationPower;
-import Snowpunk.powers.BrimstonePower;
+import Snowpunk.powers.*;
 import Snowpunk.util.Wiz;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -19,41 +16,48 @@ public class Better extends AbstractMultiUpgradeCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.POWER;
 
-    private static final int COST = 2;
-    private static final int UP_COST = 1;
-    private static final int EFFECT = 1;
+    private static final int COST = 1;
+    private static final int UP_COST = 0;
+    private static final int EFFECT = 2;
     private static final int UP_EFFECT = 1;
+    private static final int PARTS = 3;
 
-    private boolean creation = false;
-    private boolean machination = false;
+    private boolean parts = false;
 
     public Better() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseMagicNumber = magicNumber = EFFECT;
-        cardsToPreview = new Fireball();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (machination) {
-            Wiz.applyToSelf(new BetterMachinationPower(p, magicNumber));
-        } else if (creation) {
-            Wiz.applyToSelf(new BetterCreationPower(p, magicNumber));
-        } else {
-            Wiz.applyToSelf(new BetterInventionPower(p, magicNumber));
+        if (parts) {
+            Wiz.applyToSelf(new SparePartsPower(p, magicNumber));
         }
+        Wiz.applyToSelf(new BetterPower(p, magicNumber));
     }
 
     @Override
     public void addUpgrades() {
+        addUpgradeData(this, () -> {
+            this.isInnate = true;
+            if (parts) {
+                rawDescription = cardStrings.EXTENDED_DESCRIPTION[1];
+                initializeDescription();
+            } else {
+                uDesc();
+            }
+        });
         addUpgradeData(this, () -> upgradeBaseCost(UP_COST));
         addUpgradeData(this, () -> {
-            creation = true;
-            uDesc();
-        });
-        addUpgradeData(this, () -> {
-            machination = true;
-            rawDescription = cardStrings.EXTENDED_DESCRIPTION[0];
+            this.parts = true;
+            /*baseSecondMagic = secondMagic = 0;
+            upgradeSecondMagic(PARTS);*/
+            if (isInnate) {
+                rawDescription = cardStrings.EXTENDED_DESCRIPTION[1];
+            } else {
+                rawDescription = cardStrings.EXTENDED_DESCRIPTION[0];
+            }
             initializeDescription();
-        },1);
+        });
     }
 }
