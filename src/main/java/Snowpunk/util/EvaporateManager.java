ffd7030b447ad7cfa5cdc93a313c7d2382ff46b1
Alison Turner <alisonmoons@gmail.com>
2022-08-12 15:30:37 -0300

    Remove unused class

--- src/main/java/Snowpunk/util/EvaporateManager.java
+++
@@ -1,7 +0,0 @@
-package Snowpunk.util;
-
-import com.megacrit.cardcrawl.cards.CardGroup;
-
-public class EvaporateManager {
-    public static CardGroup exhaustPile = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
-}
