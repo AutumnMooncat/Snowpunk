eb708840218ac635efabbccc3c63237acb7501a2
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-02-06 02:36:07 -0400

    Add Scrap Heap relic

---
+++ src/main/java/Snowpunk/relics/ScrapHeap.java
@@ -0,0 +1,41 @@
+package Snowpunk.relics;
+
+import Snowpunk.TheConductor;
+import Snowpunk.powers.SparePartsPower;
+import Snowpunk.util.Wiz;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
+import com.megacrit.cardcrawl.ui.campfire.SmithOption;
+
+import static Snowpunk.SnowpunkMod.makeID;
+
+public class ScrapHeap extends AbstractEasyRelic {
+    public static final String ID = makeID(ScrapHeap.class.getSimpleName());
+
+    public static final int AMOUNT = 3;
+
+    public ScrapHeap() {
+        super(ID, RelicTier.SHOP, LandingSound.HEAVY, TheConductor.Enums.SNOWY_BLUE_COLOR);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + AMOUNT + DESCRIPTIONS[1];
+    }
+
+    public void atTurnStart() {
+        this.flash();
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        Wiz.applyToSelf(new SparePartsPower(Wiz.adp(), AMOUNT));
+    }
+
+    public boolean canUseCampfireOption(AbstractCampfireOption option) {
+        if (option instanceof SmithOption && option.getClass().getName().equals(SmithOption.class.getName())) {
+            ((SmithOption)option).updateUsability(false);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
