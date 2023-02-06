133481aa9161df5ccd278a1fad7d3b785417c85f
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-02-06 02:39:59 -0400

    Update Brass Pipe phrasing

--- src/main/java/Snowpunk/relics/BrassPipe.java
+++ src/main/java/Snowpunk/relics/BrassPipe.java
@@ -8,6 +8,7 @@ import static Snowpunk.SnowpunkMod.makeID;
 
 public class BrassPipe extends AbstractEasyRelic {
     public static final String ID = makeID(BrassPipe.class.getSimpleName());
+    public static final int AMOUNT = 2;
 
     public BrassPipe() {
         super(ID, RelicTier.STARTER, LandingSound.FLAT, TheConductor.Enums.SNOWY_BLUE_COLOR);
@@ -16,6 +17,11 @@ public class BrassPipe extends AbstractEasyRelic {
     @Override
     public void atBattleStartPreDraw() {
         flash();
-        Wiz.applyToSelf(new SparePartsPower(Wiz.adp(), 2));
+        Wiz.applyToSelf(new SparePartsPower(Wiz.adp(), AMOUNT));
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + AMOUNT + DESCRIPTIONS[1];
     }
 }
